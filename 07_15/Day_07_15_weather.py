99eec3fb1b23d57039f0c659615e3623a6cd601d
minju0329 <minj0329@gmail.con>
2020-07-15 16:57:23 +0900

    충청권 인공지능 프로그램

--- 07_15/Day_07_15_weather.py
+++ 07_15/Day_07_15_weather.py
@@ -1,7 +1,19 @@
 import requests
 import re
 
-url = ''
+url = 'https://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=131'
 received = requests.get(url)
-print(received)
-print(received.text)
+#print(received)
+#print(received.text)
+
+
+# 문제
+# province를 찾으세요
+print(re.findall(r'<province>+',received.text))
+
+temp = re.findall(r'<province>(.+)</province>',received.text)     #'.+'를 사용하면 어떤 글자가 들어가도 찾음, ()를 씌우면 ()안의 글자만 출력됨
+print(temp)
+print(len(temp))
+
+# 문제
+#
